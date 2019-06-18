7463c84c81ce25e3beba35593a51b1fc7a12c5dd
VivekV95 <vivekv210@gmail.com>
2019-06-18 18:52:31 -0500

    Added Swagger documentation for models

--- school/src/main/java/com/lambdaschool/school/model/Course.java
+++ school/src/main/java/com/lambdaschool/school/model/Course.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.school.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -8,16 +9,24 @@ import java.util.List;
 
 @Entity
 @Table(name = "course")
-public class Course
-{
+public class Course {
+    @ApiModelProperty(name = "courseid",
+            value = "Primary key for Course", required = true,
+            example = "1")
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 //    @JsonView(View.CoursesOnly.class)
     private long courseid;
 
-//    @JsonView(View.CoursesOnly.class)
+    //    @JsonView(View.CoursesOnly.class)
+    @ApiModelProperty(name = "coursename",
+            value = "Course Name", required = true,
+            example = "Physics")
     private String coursename;
 
+    @ApiModelProperty(name = "instructid",
+            value = "Foreign Key for Instructor",
+            example = "1")
     @ManyToOne
     @JoinColumn(name = "instructid")
     @JsonIgnoreProperties("courses")
@@ -28,58 +37,47 @@ public class Course
     @JsonIgnoreProperties("courses")
     private List<Student> students = new ArrayList<>();
 
-    public Course()
-    {
+    public Course() {
     }
 
-    public Course(String coursename)
-    {
+    public Course(String coursename) {
         this.coursename = coursename;
     }
 
-    public Course(String coursename, Instructor instructor)
-    {
+    public Course(String coursename, Instructor instructor) {
         this.coursename = coursename;
         this.instructor = instructor;
     }
 
-    public long getCourseid()
-    {
+    public long getCourseid() {
         return courseid;
     }
 
-    public void setCourseid(long courseid)
-    {
+    public void setCourseid(long courseid) {
         this.courseid = courseid;
     }
 
-    public String getCoursename()
-    {
+    public String getCoursename() {
         return coursename;
     }
 
-    public void setCoursename(String coursename)
-    {
+    public void setCoursename(String coursename) {
         this.coursename = coursename;
     }
 
-    public Instructor getInstructor()
-    {
+    public Instructor getInstructor() {
         return instructor;
     }
 
-    public void setInstructor(Instructor instructor)
-    {
+    public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
     }
 
-    public List<Student> getStudents()
-    {
+    public List<Student> getStudents() {
         return students;
     }
 
-    public void setStudents(List<Student> students)
-    {
+    public void setStudents(List<Student> students) {
         this.students = students;
     }
 }
